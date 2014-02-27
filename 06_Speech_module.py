a4f6bd3cbbb1236307a09431d7a3c654ef823442
spoty <spoty.sp@gmail.com>
2014-02-27 14:48:28 +0100

    next levls

--- 06_Speech_module.py
+++ 06_Speech_module.py
@@ -8,4 +8,25 @@ numbers.extend(tens if ones == "zero" else (tens + " " + ones)
 
 print numbers[42]
 print numbers[99]
-print numbers
+print numbers[10]
+
+# import re
+# text = "0 1 2 3 4 5 6 7 8 9".split()
+
+# # text.extend(
+
+# #     tens if ones == 0 else (tens + ones)
+# #     for tens in "1 2 3 4 5 6 7 8 9".split()
+# #     for ones in text[0:10]
+
+# #     )
+# text.extend(
+
+#     hundreds if tens == 0 else (ones + hundreds + tens)
+#     for ones in text[0:10]
+#     for hundreds in "1 2 3 4 5 6 7 8 9".split()
+#     for tens in text[0:10]
+#             )
+
+
+# print text
