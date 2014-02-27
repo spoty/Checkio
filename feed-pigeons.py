e79f2d630359a666ab153a3bdb59563a3fcb55f4
spoty <spoty.sp@gmail.com>
2014-02-27 14:50:33 +0100

    next lvls

---
+++ feed-pigeons.py
@@ -0,0 +1,25 @@
+def checkio(number):
+    x = 0
+    n = 1
+    y = 1
+    while number > 0:
+        x =  x + n
+        n = n + 1
+        number = number - x
+        print "before SUPPLY: %s, BIRDS: %s, after SUPPLY: %s" % (number + x, x, number)
+        #print "N %s" % n
+        if (number + x) < x:
+            if (number + x) < (x - n):
+                print (x - (n-1))
+                return (x - (n-1))
+            else:
+                print (number + x)
+                return (number + x)
+        if number == 0:
+            print x
+            return x
+
+
+
+
+checkio(50)
