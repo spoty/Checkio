aa2e366b6b0d2d00b4ac386a5ec76d01be95601c
spoty <spoty.sp@gmail.com>
2014-03-03 11:04:29 +0100

    cleared

--- HOME/04_House_Password.py
+++ HOME/04_House_Password.py
@@ -1,24 +1,24 @@
 import re
-list = ('aaaa5aaDaaa')
+listo = ('aaaa5aaDaaa')
 
 
-def check(list):
-    # if not list.islower():
+def check(listo):
+    # if not listo.islower():
     #     print "[OK] contains [A-Z]"
     # else:
     #     print "[NO] CAPS"
-    # if len(list) >= 10:
+    # if len(listo) >= 10:
     #     print "[OK] len OK"
     # else:
     #     print "[NO] len"
     # pattern = r'[a-z]*[A-Z][A-Za-z]*'
-    # if re.match(pattern, list):
+    # if re.match(pattern, listo):
     #     print "OK"
     # else:"
     #     print "EE
     pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.{10,}).+$'
-    if re.match(pattern, list):
+    if re.match(pattern, listo):
         print "[OK] Password is strong"
     else:
         print "[NO] Password is weak"
-check(list)
+check(listo)
