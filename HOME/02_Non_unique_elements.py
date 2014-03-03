1d96cc4b543bac77b80bab2b3454b6b5ad3447fc
spoty <spoty.sp@gmail.com>
2014-03-03 10:54:26 +0100

    simplified

--- HOME/02_Non_unique_elements.py
+++ HOME/02_Non_unique_elements.py
@@ -1,7 +1,7 @@
 def f4(seq):
    #order preserving
-    noDupes = []
-    [noDupes.append(i) for i in seq if seq.count(i) > 1]
-    return noDupes
+    return [i for i in seq if seq.count(i) > 1]
+
+
 seq = [1, 2, 3, 4, 1]
 print f4(seq)
