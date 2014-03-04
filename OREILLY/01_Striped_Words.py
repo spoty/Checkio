b7d7f3511a21dfb05eb0941ff95954b23f57ba3e
spoty <spoty.sp@gmail.com>
2014-03-04 10:50:05 +0100

    Added puzzle solution for 01_Striped_Worlds

--- OREILLY/01_Striped_Words.py
+++ OREILLY/01_Striped_Words.py
@@ -4,10 +4,16 @@ import re
 def checkio(text):
     pattern = r'^([aeiouy][bcdfghjklmnpqrstvwxz])+[aeiouy]?$|^([bcdfghjklmnpqrstvwxz][aeiouy])+[bcdfghjklmnpqrstvwxz]?$'
     listr = filter(lambda x: re.match(pattern, x),
-                   re.split(r'[,. ]+', text.lower()))
+                   re.split(r'[,. ]+', text.lower().strip('?!;')))
     return len(listr)
+
+# puzzle:
+# import re
+# checkio=lambda t:sum(any(all('@'<c and j^(c in'aeiouyAEIOUY')^i&1
+# for i,c in enumerate(w))for j in(0,1))for w in re.findall(r"\w\w+",t))
 
 
 print checkio(u"My name is ...")
 print checkio(u"Dog,cat,mouse,bird.Human.")
 print checkio("Hello world")
+print checkio("To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it?")
