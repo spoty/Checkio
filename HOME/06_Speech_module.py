ff330bb56ce6b55c82b4f40f94d38bf2e6bd34ab
spoty <spoty.sp@gmail.com>
2014-05-13 12:17:47 +0200

    style PEP8

--- HOME/06_Speech_module.py
+++ HOME/06_Speech_module.py
@@ -3,8 +3,8 @@ print numbers
 numbers.extend("ten eleven twelve thirteen fourteen fifteen sixteen".split())
 numbers.extend("seventeen eighteen nineteen".split())
 numbers.extend(tens if ones == "zero" else (tens + " " + ones)
-    for tens in "twenty thirty forty fifty sixty seventy eighty ninety".split()
-    for ones in numbers[0:10])
+               for tens in "twenty thirty forty fifty sixty seventy eighty ninety".split()
+               for ones in numbers[0:10])
 
 print numbers[42]
 print numbers[99]
@@ -13,13 +13,13 @@ print numbers[10]
 # import re
 # text = "0 1 2 3 4 5 6 7 8 9".split()
 
-# # text.extend(
+# text.extend(
 
-# #     tens if ones == 0 else (tens + ones)
-# #     for tens in "1 2 3 4 5 6 7 8 9".split()
-# #     for ones in text[0:10]
+# tens if ones == 0 else (tens + ones)
+# for tens in "1 2 3 4 5 6 7 8 9".split()
+# for ones in text[0:10]
 
-# #     )
+# )
 # text.extend(
 
 #     hundreds if tens == 0 else (ones + hundreds + tens)
