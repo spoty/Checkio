e617ca01141f1c8b9ce8c57c6f45de3a3b0fd353
spoty <spoty.sp@gmail.com>
2014-06-11 23:45:19 +0200

    Modified   ALICE/03_The_Hidden_Word.py

--- ALICE/03_The_Hidden_Word.py
+++ ALICE/03_The_Hidden_Word.py
@@ -18,21 +18,27 @@ where
 
 
 def checkio(rhyme, word):
-    poem = [x.replace(" ", "") for x in rhyme.split("\n")]
+    poem = [x.replace(" ", "").lower() for x in rhyme.split("\n")]
 
     for coo_r, x in enumerate(poem):
             for coo_w,z in enumerate(x):
                 if x[coo_w] == word[0]:
                     if x[coo_w:coo_w+len(word)] == word:
                         return [coo_r+1, coo_w+1, coo_r+1, coo_w+len(word)]
-    # print "\n".join(rhyme)
+    for coo_r, x in enumerate(map(None, *poem)):
+            for coo_w,z in enumerate(x):
+                if x[coo_w] == word[0]:
+                    if "".join(str(i) for i in x[coo_w:coo_w+len(word)]) == word:
+                        return [coo_w+1,coo_r+1,coo_w+len(word),coo_r+1]
 
 
 
-print checkio(u"""DREAMING of apples on a wall,
-And dreaming often, dear,
-I dreamed that, if I counted all,
--How many would appear?""", u"ten") == [2, 14, 2, 16]
+
+
+# print checkio(u"""DREAMING of apples on a wall,
+# And dreaming often, dear,
+# I dreamed that, if I counted all,
+# -How many would appear?""", u"ten") == [2, 14, 2, 16]
 
 # print checkio("""He took his vorpal sword in hand:
 # Long time the manxome foe he sought--
@@ -41,4 +47,8 @@ I dreamed that, if I counted all,
 # And as in uffish thought he stood,
 # The Jabberwock, with eyes of flame,
 # Came whiffling through the tulgey wood,
-# And burbled as it came!""", "noir") == [2, 14, 2, 16]
+# And burbled as it came!""", "noir") == [4, 16, 7, 16]
+
+print checkio("Hi all!\nAnd all goodbye!\nOf course goodbye.\nor not","haoo") == [1,1,4,1]
+
+print checkio("xa\nxb\nx","ab") == [1,2,2,2]
