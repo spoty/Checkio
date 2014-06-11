57db0f80b2c161d2002f5492097d98f9ad95b834
spoty <spoty.sp@gmail.com>
2014-06-11 15:50:44 +0200

    Modified   ALICE/02_Triangular_numbers.pyv

--- ALICE/02_Triangular_numbers.py
+++ ALICE/02_Triangular_numbers.py
@@ -1,24 +1,34 @@
-# from __future__ import division
-import math
+# def T_T(b,a):
+#     return ((a*(a+1)*(a+2))/6)-(b*(b+1)*(b+2)/6)+(b*(b+1)/2)
 
-# print 4.0 % 1
+# def f(n):
+#     return n*(n+1)*(n+2)/6
 
-def triangular(n):
-    return n*(n+1)/2
+# def triangular(n):
+#     return n*(n+1)/2
+
+# def checkio(n):
+#     def has_integer_cube_root(n):
+#         floatroot = (n ** (1.0 / 3.0))
+#         introot = int(round(floatroot))
+#         return (introot*introot*introot == n, introot)
+
+#     x = [i*(i+1)/2 for i in range(n)]
+
+#     if has_integer_cube_root(n)[0]: a = has_integer_cube_root(n)[1]
+#     else: return []
+#     y = triangular(a)
+#     rez = x[y-a:y+1]
+#     return rez, sum(rez)
 
 
 def checkio(n):
-    x = [i*(i+1)/2 for i in range(n)]
-    a = n**(1/3.0)
-    print int(a)
-    # y = triangular(a)
-    # rez = x[y-a:y]
-    # return rez
-
+    for start in range(1, n + 1):
+        for end in range(start, n + 1):
+            t_seq = [i*(i+1)/2 for i in range(start, end + 1)]
+            rez = sum(t_seq)
+            if rez == n: return t_seq
+            elif rez > n: break
+    return []
 
 print checkio(64)
-
-# print triangular(4)
-
-# n = 64
-# print math.pow(n,1/3)
