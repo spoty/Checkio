3a2cf1b4a5b0acc55b51ebd1a6a50014692e4f16
spoty <spoty.sp@gmail.com>
2014-06-13 21:03:18 +0200

    Modified   ALICE/03_The_Hidden_Word.py

--- ALICE/03_The_Hidden_Word.py
+++ ALICE/03_The_Hidden_Word.py
@@ -18,43 +18,41 @@ where
 
 
 def checkio(rhyme, word):
-<<<<<<< HEAD
+
     poem = [x.replace(" ", "").lower() for x in rhyme.split("\n")]
 
     for coo_r, x in enumerate(poem):
-=======
-    for coo_r, x in enumerate([x.replace(" ", "") for x in rhyme.split("\n")]):
->>>>>>> 4d787912d6c446f34cf66e16a20681c7b74108f8
+
+    #for coo_r, x in enumerate([x.replace(" ", "") for x in rhyme.split("\n")]):
+
             for coo_w,z in enumerate(x):
                 if x[coo_w] == word[0]:
                     if x[coo_w:coo_w+len(word)] == word:
                         return [coo_r+1, coo_w+1, coo_r+1, coo_w+len(word)]
-<<<<<<< HEAD
     for coo_r, x in enumerate(map(None, *poem)):
             for coo_w,z in enumerate(x):
                 if x[coo_w] == word[0]:
                     if "".join(str(i) for i in x[coo_w:coo_w+len(word)]) == word:
                         return [coo_w+1,coo_r+1,coo_w+len(word),coo_r+1]
-=======
->>>>>>> 4d787912d6c446f34cf66e16a20681c7b74108f8
 
 
 
 
 
-# print checkio(u"""DREAMING of apples on a wall,
-# And dreaming often, dear,
-# I dreamed that, if I counted all,
-# -How many would appear?""", u"ten") == [2, 14, 2, 16]
 
-# print checkio("""He took his vorpal sword in hand:
-# Long time the manxome foe he sought--
-# So rested he by the Tumtum tree,
-# And stood awhile in thought.
-# And as in uffish thought he stood,
-# The Jabberwock, with eyes of flame,
-# Came whiffling through the tulgey wood,
-# And burbled as it came!""", "noir") == [4, 16, 7, 16]
+print checkio(u"""DREAMING of apples on a wall,
+And dreaming often, dear,
+I dreamed that, if I counted all,
+-How many would appear?""", u"ten") == [2, 14, 2, 16]
+
+print checkio("""He took his vorpal sword in hand:
+Long time the manxome foe he sought--
+So rested he by the Tumtum tree,
+And stood awhile in thought.
+And as in uffish thought he stood,
+The Jabberwock, with eyes of flame,
+Came whiffling through the tulgey wood,
+And burbled as it came!""", "noir") == [4, 16, 7, 16]
 
 print checkio("Hi all!\nAnd all goodbye!\nOf course goodbye.\nor not","haoo") == [1,1,4,1]
 
