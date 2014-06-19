7b754097f2c079951d8c5d61805d416a84570e0a
spoty <spoty.sp@gmail.com>
2014-06-19 10:59:33 +0200

    Modified   ALICE/06_cakes.py

--- ALICE/06_cakes.py
+++ ALICE/06_cakes.py
@@ -1,33 +1,47 @@
-import operator
-o = {'+': operator.add, '-': operator.sub}
+from itertools import permutations
+import collections
 
 
 def checkio(k):
-    m = lambda n: set([i for i in [c[n] for c in k] if [c[n] for c in k].count(i)>2])
-    # d = [[x[0]-i, x[1]+i] for i in range(1,10)]
-    d = lambda op1,op2:[[[o[op1](x[0],i), o[op2](x[1],i)]
-            for i in range(1,10) if [o[op1](x[0],i), o[op2](x[1],i)] in k]
-            for x in k]
+    def get_line(a,b):
+        if (b[1]-a[1]) == 0:
+            m = 'h'
+            i = a[1]
+        else:
+            try:
+                m = (b[1]-a[1])/float((b[0]-a[0]))
+                i = a[1]-m*a[0]
+            except ZeroDivisionError:
+                m = 'v'
+                i = a[0]
+        return m,i
+    lines = collections.defaultdict(set)
+    for points in permutations([tuple(x) for x in k], 2):
+        line = get_line(points[0],points[1])
+        for p in points:
+            lines[line].add(p)
+
+    return len([(x[0],x[1]) for x in lines.items() if len(x[1])>2])
 
 
+# def checkio(k):
+#     def get_line(a,b):
+#         try:
+#             m = (b[1] - a[1])/float((b[0]-a[0]))
+#         except ZeroDivisionError:
+#             m = 0
+#         i = a[1] - m*a[0]
+#         return m,i
 
+#     k =[tuple(x) for x in k]
+#     # horizontal + vertical
+#     m = lambda n: set([i for i in [c[n] for c in k] if [c[n] for c in k].count(i)>2])
+#     #diagonal lines
+#     lines = collections.defaultdict(set)
+#     for points in permutations(k, 2):
+#         line = get_line(points[0],points[1])
+#         if line[0] == 0: continue
+#         for p in points:
+#             lines[line].add(p)
 
-    d1 = [x for x in d('-','+')+d('+','-') if len(x)>1]
-    d2 = [x for x in d('-','-')+d('+','+') if len(x)>1]
-    print d1
-    print d2
-    # print [[x[0]-i, x[1]+i] for i in range(1,10)] + [[x[0]+i, x[1]-i] for i in range(1,10)]
-
-    # [[x[0]-i, x[1]-i] for i in range(1,10)]
-    # [[x[0]+i, x[1]+i] for i in range(1,10)]
-
-
-
-
-    return len(m(0))+len(m(1))
-
-
-print checkio([[3, 3], [5, 5], [8, 8], [2, 8], [8, 2]]) == 2
-
-print checkio([[2, 2], [2, 5],
-[2, 8], [5, 2], [7, 2], [8, 2], [9, 2], [4, 5], [4, 8], [7, 5], [5, 8], [9, 8]]) == 6
+#     return len([x[1] for x in lines.items() if len(x[1])>2])+len(m(0))+len(m(1))
