5824a3c0656f95212eeec4e688f9c74461399eb3
spoty <spoty.sp@gmail.com>
2014-06-24 21:41:19 +0200

    Modified   ELECTRONIC STATION/05_Three_Points_Circle.py

--- ELECTRONIC STATION/05_Three_Points_Circle.py
+++ ELECTRONIC STATION/05_Three_Points_Circle.py
@@ -1,24 +1,18 @@
 import math
 def checkio(c):
     A, B, C = eval(c)
-
     a, b = 2*(B[0] - A[0]), 2*(B[1] - A[1])
     d, e = 2*(C[0] - B[0]), 2*(C[1] - B[1])
-
-    c = B[0]**2 + B[1]**2 - A[0]**2 - A[1]**2
-    f = C[0]**2 + C[1]**2 - B[0]**2 - B[1]**2
-
-    x = (b*f - e*c)/float((b*d - e*a))
-    y = (d*c - a*f)/float((b*d - e*a))
+    ab = B[0]**2 + B[1]**2 - A[0]**2 - A[1]**2
+    de = C[0]**2 + C[1]**2 - B[0]**2 - B[1]**2
+    x, y = (b*de - e*ab)/float((b*d - e*a)), (d*ab - a*de)/float((b*d - e*a))
     r = math.sqrt((x - A[0])*(x - A[0])+(y - A[1])*(y - A[1]))
-
-    x, y, r = round(x,2), round(y,2), round(r,2)
-    if x == int(x): x = int(x)
-    if y == int(y): y = int(y)
-    if r == int(r): r = int(r)
-
-    return "(x-{0})^2+(y-{1})^2={2}^2".format(str(x), str(y), str(r))
+    def m(n):
+        n = round(n,2)
+        return int(n) if n == int(n) else n
+    return "(x-{})^2+(y-{})^2={}^2".format(m(x), m(y), m(r))
 
 
 
-print checkio("(2,2),(6,2),(2,6)")
+# print checkio("(2,2),(6,2),(2,6)")
+print checkio("(7,3),(9,6),(3,6)")
