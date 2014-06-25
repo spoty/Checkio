ec436da054e2790d651d02a405e90fac5fe2c0b2
spoty <spoty.sp@gmail.com>
2014-06-25 16:27:52 +0200

    Modified   ELECTRONIC STATION/06_Number_Factory.py

--- ELECTRONIC STATION/06_Number_Factory.py
+++ ELECTRONIC STATION/06_Number_Factory.py
@@ -1,12 +1,23 @@
-def checkio(number):
-    ans, digit  = '', 9
-    while digit > 1:
-        if number%digit != 0:
-            digit -= 1
+def checkio(n):
+    a, d  = '', 9
+    while d > 1:
+        if n%d != 0:
+            d -= 1
         else:
-            ans = str(digit) + ans
-            number /= digit
-    return int(ans) if number == 1 else 0
+            a = str(d) + a
+            n /= d
+    return int(a) if n == 1 else 0
+
+# def checkio(n):
+#     if n <= 1: return n
+#     l = ""
+#     for r in "98765432":
+#         while n > 1:
+#             d, m = divmod(n, int(r))
+#             if m != 0: break
+#             n = d
+#             l = r + l
+#     return int(l) if n == 1 else 0
 
 
 print checkio(25)
