840d5a6970e96e18a7ff70fa98f36f85628da622
spoty <spoty.sp@gmail.com>
2014-06-26 09:57:16 +0200

    Modified   ELECTRONIC STATION/05_Three_Points_Circle.py

--- ELECTRONIC STATION/05_Three_Points_Circle.py
+++ ELECTRONIC STATION/05_Three_Points_Circle.py
@@ -1,13 +1,18 @@
+"""
+    Equation of line bisector of points (x0, y0), (x1, y1):
+    2(x1 - x0)x + 2(y1 - y0)y = (x1^2 - x0^2) + (y1^2 - y0^2)
+    See: http://mathhelpforum.com/pre-calculus/64489-analytic-geometry.html
+"""
 import math
 def checkio(c):
     A, B, C = eval(c)
-
+    # Form the equations of the 2 line bisectors
     a, b = 2*(B[0] - A[0]), 2*(B[1] - A[1])
     d, e = 2*(C[0] - B[0]), 2*(C[1] - B[1])
 
     c = B[0]**2 + B[1]**2 - A[0]**2 - A[1]**2
     f = C[0]**2 + C[1]**2 - B[0]**2 - B[1]**2
-
+    # Solve system using Cramer's rule
     x = (b*f - e*c)/float((b*d - e*a))
     y = (d*c - a*f)/float((b*d - e*a))
     r = math.sqrt((x - A[0])*(x - A[0])+(y - A[1])*(y - A[1]))
