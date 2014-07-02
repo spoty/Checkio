945ef5e6d8fd07fd091916e916310c1285ba16a4
spoty <spoty.sp@gmail.com>
2014-07-02 16:10:17 +0200

    Modified   INCINERATOR/02_The_Good_Radix.py

--- INCINERATOR/02_The_Good_Radix.py
+++ INCINERATOR/02_The_Good_Radix.py
@@ -30,6 +30,15 @@ print checkio(u"222") == 3
 print checkio(u"A23B") == 14
 print checkio(u"IDDQD") == 0
 
+# def checkio(str_number, radix):
+#     import string
+#     result, value = 0, (string.digits + string.ascii_uppercase)[:radix].index
+#     for exponent, digit in enumerate(reversed(str_number)):
+#         try:
+#             result += value(digit) * radix**exponent
+#         except ValueError:
+#             return -1
+#     return result
 
 # def checkio(number):
 #     max_digit = int(max(number), 36)
