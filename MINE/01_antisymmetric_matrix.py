8196cef05804c9047abe4fc4c90303b147975a1a
spoty <spoty.sp@gmail.com>
2014-07-02 16:41:05 +0200

    Added      MINE/01_antisymmetric_matrix.py

---
+++ MINE/01_antisymmetric_matrix.py
@@ -0,0 +1,19 @@
+def checkio(matrix):
+    return [[-e for e in i] for i in matrix] == [list(i) for i in zip(*matrix)]
+
+checkio = lambda m: [[-e for e in i] for i in m] == [list(i) for i in zip(*m)]
+
+
+
+print checkio([
+    [0, 1, 2],
+    [-1, 0, 1],
+    [-2, -1, 0]])
+print checkio([
+    [0, 1, 2],
+    [-1, 1, 1],
+    [-2, -1, 0]])
+print checkio([
+    [0, 1, 2],
+    [-1, 0, 1],
+    [-3, -1, 0]])
