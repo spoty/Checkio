e615254489d6839b23fddb2d218536915338cf96
spoty <spoty.sp@gmail.com>
2014-07-05 16:33:46 +0200

    updated

--- MINE/02_flatten.py
+++ MINE/02_flatten.py
@@ -3,6 +3,24 @@ import re
 def flat_list(l):
     return [int(x) for x in re.sub(r'[\[,\]]', ' ', str(l)).split()]
 
+def flat_list(l):
+    return list(map(int, re.findall('-?\d+', str(l))))
+
+def flat_list(l):
+    r = []
+    def f(l):
+        for i in l:
+            r.append(i) if type(i) is int else f(i)
+    f(l)
+    return r
+
+flat_list=f=lambda d:0*d==0 and[d]or sum(map(f,d),[])
+
+flat_list=lambda array: eval('['+str(array).translate(str.maketrans('','','[]'))+']')
+
+f = lambda x: x and f(x[0]) + f(x[1:]) if isinstance(x, list) else [x]
+flat_list = f
+
 #regex
 flat_list = lambda l: [int(x) for x in re.sub(r'[\[,\]]', ' ', str(l)).split()]
 
