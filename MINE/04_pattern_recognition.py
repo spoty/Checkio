26ae3ef2073afbbee1a6836d2299ad41819e811a
spoty <spoty.sp@gmail.com>
2014-07-05 23:55:03 +0200

    Modified   MINE/04_pattern_recognition.py

--- MINE/04_pattern_recognition.py
+++ MINE/04_pattern_recognition.py
@@ -13,70 +13,68 @@ Precondition:
 V x from data : x == 0 or x == 1
 """
 
-
-def find(haystack, needle):
-    """Return the index at which the sequence needle appears in the
-    sequence haystack, or -1 if it is not found, using the Boyer-
-    Moore-Horspool algorithm. The elements of needle and haystack must
-    be hashable.
-
-    >>> find([1, 1, 2], [1, 2])
-    1
-
-    """
-    h = len(haystack)
-    n = len(needle)
-    skip = {needle[i]: n - i - 1 for i in range(n - 1)}
-    i = n - 1
-    while i < h:
-        for j in range(n):
-            if haystack[i - j] != needle[-j - 1]:
-                i += skip.get(haystack[i], n)
-                break
-        else:
-            return (i - n + 1), (i - n + 1) + 2
-    return 0
+def find(pattern, row, L=len):
+    return [(index, index + L(pattern))
+                for index in (i for i, e in enumerate(row) if e == pattern[0])
+                if row[index:index + L(pattern)] == pattern]
 
 
 def checkio(pattern, image):
-    N = len(pattern[0])
+    N = len(pattern)
+    d = {i: find(pattern[0], row) for i, row in enumerate(image)
+         if find(pattern[0], row) != []}
+    for k in d:
+        for x in d[k]:
+            a, b = x
+            if [f[a:b] for f in image[k:k + N]] == pattern:
+                for row in image[k:k + N]:
+                    row[a:b] = map(lambda x: 2 if x == 0 else 3, row[a:b])
+    return image
 
+print checkio([[1, 0], [1, 1]],
+              [[0, 1, 0, 1, 0],
+               [0, 1, 1, 0, 0],
+               [1, 0, 1, 1, 0],
+               [1, 1, 0, 1, 1],
+               [0, 1, 1, 0, 0]]) == [[0, 3, 2, 1, 0],
+                                     [0, 3, 3, 0, 0],
+                                     [3, 2, 1, 3, 2],
+                                     [3, 3, 0, 3, 3],
+                                     [0, 1, 1, 0, 0]]
+print checkio([[1, 1], [1, 1]],
+              [[1, 1, 1],
+               [1, 1, 1],
+               [1, 1, 1]]) == [[3, 3, 1],
+                               [3, 3, 1],
+                               [1, 1, 1]]
 
+print checkio([[0, 1, 0], [1, 1, 1]],
+              [[0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+               [0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+               [0, 1, 0, 0, 1, 1, 1, 0, 1, 0],
+               [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+               [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+               [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+               [0, 1, 1, 0, 0, 0, 1, 1, 1, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) == [[0, 2, 3, 2, 0, 0, 0, 2, 3, 2],
+                                                    [0, 3, 3, 3, 0, 0, 0, 3, 3, 3],
+                                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                    [0, 0, 0, 0, 2, 3, 2, 0, 0, 0],
+                                                    [2, 3, 2, 0, 3, 3, 3, 0, 1, 0],
+                                                    [3, 3, 3, 0, 0, 0, 0, 0, 1, 1],
+                                                    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+                                                    [0, 0, 1, 0, 0, 0, 2, 3, 2, 0],
+                                                    [0, 1, 1, 0, 0, 0, 3, 3, 3, 0],
+                                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-    for i, row in enumerate(image):
-
-        for p in range(5):
-            print p
-            if find(row, pattern[0]) != 0:
-                a, b = find(row, pattern[0])
-
-                if [f[a:b] for f in image[i:i + N] if a + b > 0] == pattern:
-                    for row in image[i:i + N]:
-                        row[a:b] = map(lambda x: 2 if x == 0 else 3, row[a:b])
-
-
-
-
-            # for f in image[i:i+N]:
-            #     print f[a:b]
-
-    for x in image:
-        print x
-    return []
-
-checkio([[1, 0], [1, 1]],
-        [[0, 1, 0, 1, 0],
-         [0, 1, 1, 0, 0],
-         [1, 0, 1, 1, 0],
-         [1, 1, 0, 1, 1],
-         [0, 1, 1, 0, 0]]) == [[0, 3, 2, 1, 0],
-                               [0, 3, 3, 0, 0],
-                               [3, 2, 1, 3, 2],
-                               [3, 3, 0, 3, 3],
-                               [0, 1, 1, 0, 0]]
-# checkio([[1, 1], [1, 1]],
-#         [[1, 1, 1],
-#          [1, 1, 1],
-#          [1, 1, 1]]) == [[3, 3, 1],
-#                          [3, 3, 1],
-#                          [1, 1, 1]]
+# def find(pattern, row):
+#     r = []
+#     N = len(pattern)
+#     for index in (i for i, e in enumerate(row) if e == pattern[0]):
+#         if row[index:index + N] == pattern:
+#             r.append((index, index + N))
+#     [index for index in (i for i, e in enumerate(row) if e == pattern[0])
+#      if row[index:index + N] == pattern]
+#     return r
