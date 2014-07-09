5a03117b80e0331cc8ffb1ffe53776593c3dc041
spoty <spoty.sp@gmail.com>
2014-07-09 23:41:42 +0200

    Added      MINE/14_Palindromic_palindrome.py

---
+++ MINE/14_Palindromic_palindrome.py
@@ -0,0 +1,15 @@
+# def checkio(s):
+#     return s == s[::-1]
+
+# "a\";w=]1-::[w trinp;)(tupni_war=w;";w=raw_input();print w[::-1]==w;"\a"
+# checkio
+# s==s[::-1]
+# #checkio = lambda s:
+
+
+# checkio=lambda s:s==s[::-1];
+
+
+"a\";]1-::[s==s:s adbmal=oikcehc;";checkio=lambda s:s==s[::-1];"\a"
+
+print checkio('sas')
