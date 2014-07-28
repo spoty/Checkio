383aa8c2b6e1858a1d629094f10b22aee31482cf
spoty <spoty.sp@gmail.com>
2014-07-28 21:51:02 +0200

    Added      HOME/Roman_numerals.py

---
+++ HOME/Roman_numerals.py
@@ -0,0 +1,22 @@
+numeral_map = tuple(zip(
+    (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+    ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')))
+
+
+def int_to_roman(i):
+    result = []
+    for integer, numeral in numeral_map:
+        count = i // integer
+        result.append(numeral * count)
+        i -= integer * count
+    return ''.join(result)
+
+def roman_to_int(n):
+    i = result = 0
+    for integer, numeral in numeral_map:
+        while n[i:i + len(numeral)] == numeral:
+            result += integer
+            i += len(numeral)
+    return result
+
+print int_to_roman(800)
